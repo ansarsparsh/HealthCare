62086a2af286083b8c03b8386c9f07c7282cef96
Ansar Ali <ansar4wnt@gmail.com>
2024-12-13 11:55:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HealthCare from './HealthCare';
+
+function App() {
+  return (
+    <div className="App">
+       <HealthCare></HealthCare>
+    </div>
+  );
+}
+
+export default App;
